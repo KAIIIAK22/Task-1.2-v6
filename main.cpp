954b14cae409147682cfe69f6e139bae8a355c56
Pavel Poltaves <dzhura17@yandex.ru>
2019-08-02 19:32:26 +0300

    Some Changes

--- main.cpp
+++ main.cpp
@@ -1,26 +1,29 @@
 #include <iostream>
 using namespace std;
 
-   int main()
-   {
-      unsigned  int a ;
-      cout<<"Vasha zarplata($): ";
-      cin >>a;
-      try{
-      if (a>1000)
-       {
-          if (a>1000000){
-          cout <<"Ti millioner!";
-          throw 0;
-       }
-      cout <<"Horosho";
-      throw 0;
-     }
-     cout <<"Rabotai bolshe";
-     throw 0;
-     }
-     catch(int d){
-     cout <<"...no ti molodec";
-     return d;
+int main()
+{
+    unsigned  int a ;
+    cout<<"Vasha zarplata($): ";
+    cin >>a;
+    try
+    {
+        if (a>1000)
+        {
+            if (a>1000000)
+            {
+                cout <<"Ti millioner!";
+                throw 0;
+            }
+            cout <<"Horosho";
+            throw 0;
+        }
+        cout <<"Rabotai bolshe";
+        throw 0;
     }
-   }
+    catch(int d)
+    {
+        cout <<"...no ti molodec";
+        return d;
+    }
+}
